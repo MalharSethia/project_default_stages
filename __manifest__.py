760634d53e43e8a2ba89c3cae45aefb11314e668
MalharSethia <mal.sethia@gmail.com>
2025-07-04 13:16:32 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Project Default Stages',
+    'version': '17.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Automatically create default stages for new projects',
+    'description': """
+        This module automatically creates default stages (BACKLOG, TO-DO, IN PROGRESS, 
+        WAITING, BLOCKED, QAT, UAT, DONE) when a new project is created.
+        The stages are fully editable and can be customized per project.
+    """,
+    'author': 'Your Company',
+    'depends': ['project'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
